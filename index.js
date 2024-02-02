499110cac59b0a19c66a9b2a4b3c5882555f879f
michael <abdulazizklichev1@gmail.com>
2024-02-02 13:04:41 +0900

    fix:filePackage

--- index.js
+++ index.js
@@ -1,64 +1,91 @@
-//  Module package CORE => node.js ni ichiga install qilib berilgan packagelar modeullar
-setTimeout(function(){
- console.log("ishga tushdi");
-},1000); // 2ta parametr oladi: 1-function, 2-Time ni oladi
-let number = 0;
-setInterval(() => {
-    console.log("hisob" , number);
-    number++;
-}, 0);
- const fs = require('fs');  //core packagelarini call qilishimiz kerak (require) fs objetni bir qator methodlari , documention buyicha 2ta parametr pass qilinadi 1:"start": "node index.js",2: utf protokoli qullanildi
- const data = fs.readFileSync('./input.txt','utf8');  
+// //  Module package CORE => node.js ni ichiga install qilib berilgan packagelar modeullar
+// setTimeout(function(){
+//  console.log("ishga tushdi");
+// },1000); // 2ta parametr oladi: 1-function, 2-Time ni oladi
+// let number = 0;
+// setInterval(() => {
+//     console.log("hisob" , number);
+//     number++;
+// }, 0);
+//  const fs = require('fs');  //core packagelarini call qilishimiz kerak (require) fs objetni bir qator methodlari , documention buyicha 2ta parametr pass qilinadi 1:"start": "node index.js",2: utf protokoli qullanildi
+//  const data = fs.readFileSync('./input.txt','utf8');  
 
-console.log(data);
-console.log("***************");
-
-
-
-
-fs.writeFileSync("./input.txt",`${data} \n\t\t by Michael`);
-const new_data = fs.readFileSync("./input.txt","utf8");  
-
-console.log(new_data);
+// console.log(data);
+// console.log("***************");
 
 
 
-//  Module package EXTERNAL => foydalishdan oldin install qilinadi
-const moment = require('moment');
 
-setInterval(() => {
-    const time = moment().format();
-    console.log(`hozirgi vaqt ${time}`);
-},5000);
+// fs.writeFileSync("./input.txt",`${data} \n\t\t by Michael`);
+// const new_data = fs.readFileSync("./input.txt","utf8");  
 
-//inquirer package
-const inquirer = require("inquirer");
-inquirer
-.prompt([{type: "input", name: "raqam", message:"raqamni kiriting?"}])
-.then((answer) =>{
-console.log("man kiritgan raqam qiymati:", answer.raqam);
-})
-.catch((err) => console.log(err));
+// console.log(new_data);
 
 
-const validator = require("validator");
- const test = validator.isInt("100");
- const test1 = validator.isIP("175.208.119.125");
+
+// //  Module package EXTERNAL => foydalishdan oldin install qilinadi
+// const moment = require('moment');
+
+// setInterval(() => {
+//     const time = moment().format();
+//     console.log(`hozirgi vaqt ${time}`);
+// },5000);
+
+// //inquirer package
+// const inquirer = require("inquirer");
+// inquirer
+// .prompt([{type: "input", name: "raqam", message:"raqamni kiriting?"}])
+// .then((answer) =>{
+// console.log("man kiritgan raqam qiymati:", answer.raqam);
+// })
+// .catch((err) => console.log(err));
+
+
+// const validator = require("validator");
+//  const test = validator.isInt("100");
+//  const test1 = validator.isIP("175.208.119.125");
 
        
  
- console.log("test" , test1);
+//  console.log("test" , test1);
 
 
 
-const { v4: uuidv4} = require('uuid');
-const random = uuidv4();
-console.log('random',random)
-const chalk = require("chalk");
-const log  = console.log;
-const log1  = console.log;
-log1(chalk.blue("Hello") + random + chalk.red("!"));
-log(chalk.red(`uuid creates ${random}`));
+// const { v4: uuidv4} = require('uuid');
+// const random = uuidv4();
+// console.log('random',random)
+// const chalk = require("chalk");
+// const log  = console.log;
+// const log1  = console.log;
+// log1(chalk.blue("Hello") + random + chalk.red("!"));
+// log(chalk.red(`uuid creates ${random}`));
 
 // // // Module package FILE
 // // // ^8.2.4
+
+// const calculate = require("./hisob.js");
+ //const moment
+
+//  const natija = calculate.kopaytirish(80,  20);
+//  console.log("Natija",natija);
+
+//  console.log('==================');
+
+//  const natija2 = calculate.ayirish(80,  20);
+//  console.log("Natija",natija2);
+
+
+// console.log(require("module").wrapper);
+//  console.log(arguments);
+
+const Account =require("./account");
+Account.tellMeAboutClass();
+Account.tellMeTime();
+console.log("=============");
+const myAccaunt = new Account('Michael', 9000000, 45435454389563);
+myAccaunt.giveMeDetails();
+
+myAccaunt.makeDeposit(1000000);
+
+// Ferrari 3,4mln usd
+myAccaunt.withdrowMoney(34000000);
